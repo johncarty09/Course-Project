8e539a934810711f51f5281bfe78a6e4fc304745
Travis Gajewski <t_gajewski@comcast.net>
2018-10-04 21:49:27 -0400

    UI first run

--- CISS_311_Course_Project/Form1.Designer.cs
+++ CISS_311_Course_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CISS_311_Course_Project
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "mainForm";
+            this.Text = "Library Management System";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
