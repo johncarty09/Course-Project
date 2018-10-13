92faefdd756df995d15304f9fd034d0dba26b05f
Travis Gajewski <t_gajewski@comcast.net>
2018-10-13 17:28:42 -0400

    Directory form first pass

--- CISS_311_Course_Project/Directory.Designer.cs
+++ CISS_311_Course_Project/Directory.Designer.cs
@@ -28,12 +28,309 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.detailsListBox = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtShelfNumber = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtMiddle = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(432, 220);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(146, 41);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(20, 220);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(146, 41);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // detailsListBox
+            // 
+            this.detailsListBox.FormattingEnabled = true;
+            this.detailsListBox.Location = new System.Drawing.Point(12, 272);
+            this.detailsListBox.Name = "detailsListBox";
+            this.detailsListBox.Size = new System.Drawing.Size(579, 121);
+            this.detailsListBox.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(238, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "First:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Shelf No.:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(236, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Qty.:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(19, 32);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Last:";
+            // 
+            // txtShelfNumber
+            // 
+            this.txtShelfNumber.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtShelfNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtShelfNumber.Location = new System.Drawing.Point(66, 129);
+            this.txtShelfNumber.Name = "txtShelfNumber";
+            this.txtShelfNumber.Size = new System.Drawing.Size(79, 26);
+            this.txtShelfNumber.TabIndex = 13;
+            this.txtShelfNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtYear.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtYear.Location = new System.Drawing.Point(189, 129);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(40, 26);
+            this.txtYear.TabIndex = 12;
+            this.txtYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtQuantity.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtQuantity.Location = new System.Drawing.Point(271, 129);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(40, 26);
+            this.txtQuantity.TabIndex = 14;
+            this.txtQuantity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtISBN.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtISBN.Location = new System.Drawing.Point(317, 24);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(249, 26);
+            this.txtISBN.TabIndex = 11;
+            this.txtISBN.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtLastName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtLastName.Location = new System.Drawing.Point(55, 25);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(177, 26);
+            this.txtLastName.TabIndex = 8;
+            this.txtLastName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(456, 31);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "M.I.";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMiddle);
+            this.groupBox2.Controls.Add(this.txtFirstName);
+            this.groupBox2.Controls.Add(this.txtLastName);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(9, 55);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(557, 67);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Author";
+            // 
+            // txtMiddle
+            // 
+            this.txtMiddle.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtMiddle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtMiddle.Location = new System.Drawing.Point(487, 24);
+            this.txtMiddle.Name = "txtMiddle";
+            this.txtMiddle.Size = new System.Drawing.Size(40, 26);
+            this.txtMiddle.TabIndex = 10;
+            this.txtMiddle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtFirstName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtFirstName.Location = new System.Drawing.Point(273, 25);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(177, 25);
+            this.txtFirstName.TabIndex = 9;
+            this.txtFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(224, 220);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(146, 41);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear Form";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtShelfNumber);
+            this.groupBox1.Controls.Add(this.txtYear);
+            this.groupBox1.Controls.Add(this.txtQuantity);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtISBN);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txtTitle);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(580, 191);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Book Details";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtTitle.Location = new System.Drawing.Point(64, 24);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(206, 26);
+            this.txtTitle.TabIndex = 2;
+            this.txtTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Title:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ISBN:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 11;
+            // 
+            // Directory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(609, 408);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.detailsListBox);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "Directory";
             this.Text = "Directory";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox detailsListBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label txtShelfNumber;
+        private System.Windows.Forms.Label txtYear;
+        private System.Windows.Forms.Label txtQuantity;
+        private System.Windows.Forms.Label txtISBN;
+        private System.Windows.Forms.Label txtLastName;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label txtMiddle;
+        private System.Windows.Forms.Label txtFirstName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label txtTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
