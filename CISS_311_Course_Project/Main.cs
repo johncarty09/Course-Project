1ddb24a940d9d687cbd3bf8ada383c5804d9b1c0
John Carty <john.carty@gmail.com>
2018-10-14 18:43:38 -0400

    buttons now open forms. still exit to handle exit button.

--- CISS_311_Course_Project/Main.cs
+++ CISS_311_Course_Project/Main.cs
@@ -19,7 +19,42 @@ namespace CISS_311_Course_Project
 
         private void btnNew_Click(object sender, EventArgs e)
         {
-            
+            this.Hide();
+            NewBook form2 = new NewBook();
+
+            //Register the update event
+            form2.updateEvent += new EventHandler(handleUpdateEvent);
+
+            //Register form closed event
+            form2.FormClosed += new FormClosedEventHandler(form2_FormClosed);
+
+            form2.Show();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Directory form2 = new Directory();
+            form2.Show();
+
+            //Register the update event
+            form2.updateEvent += new EventHandler(handleUpdateEvent);
+
+            //Register form closed event
+            form2.FormClosed += new FormClosedEventHandler(form2_FormClosed);
+
+            form2.Show();
+        }
+
+        void form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Show();
+        }
+
+        //Handler for the event from form 2
+        void handleUpdateEvent(object sender, EventArgs e)
+        {
+            this.Show();
         }
     }
 }
